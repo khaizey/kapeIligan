1576d888f1f033959e7381f9231a2fe3bf2d85c9
clearglass <joshuadave143@gmail.com>
2017-10-29 07:59:51 -0700

    new

--- invent/raw.php
+++ invent/raw.php
@@ -28,14 +28,14 @@
   <!-- Navigation-->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
     <img src="../images/logo.png" style="height:50px;padding-right:10px;" alt="logo">
-    <a class="navbar-brand" href="index.php">Kape Iligan</a>
+    <a class="navbar-brand" href="../index.php">Kape Iligan</a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarResponsive">
       <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
         <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
-          <a class="nav-link" href="index.php">
+          <a class="nav-link" href="../index.php">
             <i class="fa fa-fw fa-dashboard"></i>
             <span class="nav-link-text">Dashboard</span>
           </a>
