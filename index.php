5f1cbdaf6c4e5fb8db6576ca67e78c8e8f2e292b
clearglass <joshuadave143@gmail.com>
2017-10-29 07:50:06 -0700

    change

--- index.php
+++ index.php
@@ -80,7 +80,7 @@
         <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Link">
           <a class="nav-link" href="#">
             <i class="fa fa-fw fa-link"></i>
-            <span class="nav-link-text">wala pa</span>
+            <span class="nav-link-text">hapt na</span>
           </a>
         </li>
       </ul>
