80e32fab4d65a9060d6b76fa46f5dbd1701b0831
khaizey <33142248+khaizey@users.noreply.github.com>
2017-10-29 11:48:20 +0800

    roasting

--- roasting.php
+++ roasting.php
@@ -107,7 +107,7 @@
       <!-- Example DataTables Card-->
       <div class="card mb-3">
         <div class="card-header">
-          <i class="fa fa-table"></i></div>
+          <i class="fa fa-table"></i> Roasting Process</div>
         <div class="card-body">
           <div class="table-responsive">
 <!-- begin table -->
