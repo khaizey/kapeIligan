dcae5a6badec17f25acb36f93e357dc763d213b8
clearglass <joshuadave143@gmail.com>
2017-10-29 17:29:29 -0700

    v

--- index.php
+++ index.php
@@ -176,7 +176,7 @@
       <!-- Area Chart Example-->
       <div class="card mb-3">
         <div class="card-header">
-          <i class="fa fa-area-chart"></i> Area Chart Example</div>
+          <i class="fa fa-area-chart"></i> Inventory indicator</div>
         <div class="card-body">
           <canvas id="myAreaChart" width="100%" height="30"></canvas>
         </div>
